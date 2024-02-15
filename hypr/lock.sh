8dc41a859c9f5d8042897718029a4245e3d1f83d
Dakota Marshall <me@dakotamarshall.net>
2024-02-15 15:36:30 -0500

    improve lockscreen

--- hypr/lock.sh
+++ hypr/lock.sh
@@ -1,5 +1,5 @@
 swaylock \
-	--screenshots \
+	--image ~/Pictures/Wallpapers/skull.png \
 	--clock \
 	--indicator \
 	--indicator-radius 100 \
@@ -7,10 +7,16 @@ swaylock \
 	--effect-blur 7x5 \
 	--effect-vignette 0.5:0.5 \
 	--ring-color bb00cc \
-	--key-hl-color 880033 \
+	--key-hl-color 74c7ec \
 	--line-color 00000000 \
-	--inside-color 00000088 \
+	--ring-color cba6f7 \
+	--inside-color 11111b88 \
+	--inside-ver-color 1e1e2e88 \
+	--ring-ver-color 89dceb \
+	--inside-wrong-color f38ba888 \
 	--separator-color 00000000 \
+	--text-color cba6f7 \
+	--text-ver-color cdd6f4 \
 	--grace 2 \
 	--fade-in 0.2 \
   --daemonize
