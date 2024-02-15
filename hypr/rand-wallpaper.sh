a0d4c23de9bb6b76a54e53c18d4cbbd33f439f93
Dakota Marshall <me@dakotamarshall.net>
2024-02-15 11:31:18 -0500

    randomize wallpaper, and create bind for reload

---
+++ hypr/rand-wallpaper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+directory=~/Pictures/Wallpapers
+monitors=`hyprctl monitors | grep Monitor | awk '{print $2}'`
+
+if [ -d "$directory" ]; then
+    random_background=$(ls $directory/* | shuf -n 1)
+
+    hyprctl hyprpaper unload all
+    hyprctl hyprpaper preload $random_background
+
+    while IFS= read -r monitor; do
+        hyprctl hyprpaper wallpaper "$monitor, contain:$random_background"
+    done <<< "$monitors"
+
+fi
