cb978d339646b35670aa588c02cfa2c19e9750fa
Dakota Marshall <me@dakotamarshall.net>
2024-04-25 15:14:56 -0400

    add theme settings for catppuccin

--- nixos/modules/home-manager/dmarshall/theming.nix
+++ nixos/modules/home-manager/dmarshall/theming.nix
@@ -5,6 +5,18 @@
         enable = true;
         theme = {
             name = "Catppuccin-Mocha-Standard-Lavender-Dark";
+            package = pkgs.catppuccin-gtk.override {
+                accents = [ "lavender" ];
+                size = "standard";
+                variant = "mocha";
+            };
         };
+    };
+
+    # Now symlink the `~/.config/gtk-4.0/` folder declaratively:
+    xdg.configFile = {
+      "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+      "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+      "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
     };
 }
