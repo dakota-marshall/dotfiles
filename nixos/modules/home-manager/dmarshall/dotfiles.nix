b00d0a94f12df237f789ca7fb6af386ef0f39f0a
Dakota Marshall <me@dakotamarshall.net>
2024-04-25 15:02:19 -0400

    add semicolon to end of symlinks

--- nixos/modules/home-manager/dmarshall/dotfiles.nix
+++ nixos/modules/home-manager/dmarshall/dotfiles.nix
@@ -4,16 +4,16 @@
 
   xdg.configFile = {
     "obs-studio".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/obs-studio";
-    "terminator".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/terminator"
-    "hypr".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/hypr"
-    "swayidle".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/swayidle"
-    "waybar".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/waybar"
-    "wofi".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/wofi"
-    "swaync".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/swaync"
-    "nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/nvim"
-    "Yubico".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/yubico"
-    "OpenTabletDriver".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/OpenTabletDriver"
-    "openfortivpn".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/openfortivpn"
-    "blender".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/"
+    "terminator".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/terminator";
+    "hypr".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/hypr";
+    "swayidle".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/swayidle";
+    "waybar".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/waybar";
+    "wofi".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/wofi";
+    "swaync".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/swaync";
+    "nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/nvim";
+    "Yubico".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/yubico";
+    "OpenTabletDriver".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/OpenTabletDriver";
+    "openfortivpn".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/openfortivpn";
+    "blender".source = config.lib.file.mkOutOfStoreSymlink "/home/dmarshall/gitlab/dakota.marshall/dotfiles/dotfiles/";
   };
 }
