43860f10005fbba2c347d4d9101031926bba924b
Dakota Marshall <me@dakotamarshall.net>
2024-06-26 15:53:09 -0400

    Add catppuccin module

--- nixos/modules/home-manager/dmarshall/theming.nix
+++ nixos/modules/home-manager/dmarshall/theming.nix
@@ -1,26 +1,44 @@
 { config, pkgs, ... }:
 
 {
-    gtk = {
+    # Enable catppuccin for everything
+    catppuccin = {
         enable = true;
-        theme = {
-            name = "Catppuccin-Mocha-Standard-Lavender-Dark";
-            package = pkgs.catppuccin-gtk.override {
-                accents = [ "lavender" ];
-                size = "standard";
-                variant = "mocha";
-            };
-        };
-        iconTheme = {
-          package = pkgs.gnome.adwaita-icon-theme;
-          name = "Adwaita";
+        accent = "lavender";
+        flavor = "mocha";
+
+        pointerCursor = {
+            enable = true;
+            accent = "lavender";
+            flavor = "mocha";
         };
     };
 
-    # Now symlink the `~/.config/gtk-4.0/` folder declaratively:
-    xdg.configFile = {
-      "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-      "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-      "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+    gtk = {
+        enable = true;
+        catppuccin = {
+            enable = true;
+            accent = "lavender";
+            flavor = "mocha";
+        };
+        # theme = {
+        #     name = "Catppuccin-Mocha-Standard-Lavender-Dark";
+        #     package = pkgs.catppuccin-gtk.override {
+        #         accents = [ "lavender" ];
+        #         size = "standard";
+        #         variant = "mocha";
+        #     };
+        # };
+        # iconTheme = {
+        #   package = pkgs.gnome.adwaita-icon-theme;
+        #   name = "Adwaita";
+        # };
     };
+
+    # Now symlink the `~/.config/gtk-4.0/` folder declaratively:
+    # xdg.configFile = {
+    #   "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    #   "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    #   "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+    # };
 }
