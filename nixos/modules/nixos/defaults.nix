0a5d6837dabb2e2ccd2c6bdadbc29480bdcce50c
Dakota Marshall <me@dakotamarshall.net>
2024-07-31 18:29:53 -0400

    remove deprecated sounds definiton

--- nixos/modules/nixos/defaults.nix
+++ nixos/modules/nixos/defaults.nix
@@ -59,7 +59,6 @@
   virtualisation.libvirtd.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
